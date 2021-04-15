cd02629888f56e4718694a0e8603e5da75f13628
Alex <atonkonogov.services@gmail.com>
2021-04-16 02:13:45 +0300

    homework4

---
+++ homework4/task3/questions.js
@@ -0,0 +1,233 @@
+var questions = [
+{
+    question: 'На каком инструменте, как считается, играл древнерусский певец и сказитель?',
+    cost: 1000,
+    variants: [
+        {
+            text: 'Боян',
+            right: true
+        },
+        {
+            text: 'На виолончели',
+            right: false
+        },
+        {
+            text: 'На баяне',
+            right: false
+        },
+        {
+            text: 'На гитаре',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'В какой из этих стран один из официальных языков - французский?',
+    cost: 5000,
+    variants: [
+        {
+            text: 'Республика Гаити',
+            right: true
+        },
+        {
+            text: 'Кения',
+            right: false
+        },
+        {
+            text: 'Эквадор',
+            right: false
+        },
+        {
+            text: 'Венесуэла',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'В каком из этих фильмов не снимался Александр Абдулов?',
+    cost: 10000,
+    variants: [
+        {
+            text: '"Карнавал"',
+            right: false
+        },
+        {
+            text: '"Московские каникулы"',
+            right: true
+        },
+        {
+            text: '"Чародеи"',
+            right: false
+        },
+        {
+            text: '"Убить дракона"',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'В каком году произошла Куликовская битва?',
+    cost: 50000,
+    variants: [
+        {
+            text: '1380',
+            right: true
+        },
+        {
+            text: '1569',
+            right: false
+        },
+        {
+            text: '1616',
+            right: false
+        },
+        {
+            text: '1773',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'Кто автор музыки к детской песенке Чунга-Чанга?',
+    cost: 75000,
+    variants: [
+        {
+            text: 'Шаинский',
+            right: true
+        },
+        {
+            text: 'Зацепин',
+            right: false
+        },
+        {
+            text: 'Дербенёв',
+            right: false
+        },
+        {
+            text: 'Шпиц',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'Какая картина Малевича находится в Русском музее?',
+    cost: 100000,
+    variants: [
+        {
+            text: 'Красный квадрат',
+            right: true
+        },
+        {
+            text: 'Белый квадрат',
+            right: false
+        },
+        {
+            text: 'Чёрный квадрат',
+            right: false
+        },
+        {
+            text: 'Точильщик',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'Шкала Сковилла - это шкала оценки...',
+    cost: 125000,
+    variants: [
+        {
+            text: 'Качества атмосферного воздуха',
+            right: false
+        },
+        {
+            text: 'Привлекательности женщин',
+            right: false
+        },
+        {
+            text: 'Остроты перца',
+            right: true
+        },
+        {
+            text: 'Уровня моря',
+            right: false
+        }
+
+    ]
+},
+
+{
+    question: 'Какой титул имел Дон Кихот?',
+    cost: 159000,
+    variants: [
+        {
+            text: 'Барон',
+            right: false
+        },
+        {
+            text: 'Маркиз',
+            right: false
+        },
+        {
+            text: 'Идальго',
+            right: true
+        },
+        {
+            text: 'Вождь',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'Кто автор антиутопического романа "О дивный новый мир"',
+    cost: 175000,
+    variants: [
+        {
+            text: 'Олдос Хаксли',
+            right: true
+        },
+        {
+            text: 'Рэй Брэдбери',
+            right: false
+        },
+        {
+            text: 'Джордж Оруэлл',
+            right: false
+        },
+        {
+            text: 'Сомерсет Моэм',
+            right: false
+        }
+
+    ]
+},
+{
+    question: 'Как называется самая глубокая точка поверхности Земли, находящаяся на дне Марианской впадины?',
+    cost: 300000,
+    variants: [
+        {
+            text: 'Филиппинская плита',
+            right: false
+        },
+        {
+            text: 'Бездна Челленджера',
+            right: true
+        },
+        {
+            text: 'Кермадек',
+            right: false
+        },
+        {
+            text: 'Черное Логово',
+            right: false
+        }
+
+    ]
+}
+];
